3b09ec63d9a871a94ee522d69356c27c5bc409a2
jayasivapriya <sivapriya2707@gmail.com>
2026-03-08 17:38:44 +0530

    Create LC_342_Power_of_Four.cpp

---
+++ Math/LC_342_Power_of_Four.cpp
@@ -0,0 +1,32 @@
+/*
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+Example 1:
+Input: n = 16
+Output: true
+  
+Example 2:
+Input: n = 5
+Output: false
+
+Example 3:
+Input: n = 1
+Output: true
+  
+Constraints:
+-2^31 <= n <= 2^31 - 1
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0)
+        return true;
+        else
+        return false;
+    }
+};

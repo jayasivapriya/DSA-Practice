c9e99fb245872ddfd3fe4086c22495a8c81607f2
jayasivapriya <sivapriya2707@gmail.com>
2026-03-08 17:28:44 +0530

    Create LC_189_Rotate_Array.cpp

---
+++ Arrays/LC_189_Rotate_Array.cpp
@@ -0,0 +1,37 @@
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+  
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+Constraints:
+1 <= nums.length <= 105
+-2^31 <= nums[i] <= 2^31 - 1
+0 <= k <= 10^5
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+  */
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        reverse(nums.begin(), nums.begin() + (n - k));
+        reverse(nums.begin() + (n - k), nums.begin() + n);
+        reverse(nums.begin(), nums.begin() + n);
+    }
+};

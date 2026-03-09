345f7accdc499d6152a639bb5b0ca0f37063416d
jayasivapriya <sivapriya2707@gmail.com>
2026-03-09 17:40:05 +0530

    Create LC_283_Move_Zeroes.cpp

---
+++ Arrays/LC_283_Move_Zeroes.cpp
@@ -0,0 +1,42 @@
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+  
+Constraints:
+1 <= nums.length <= 10^4
+-2^31 <= nums[i] <= 2^31 - 1
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = -1;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+        if(j == -1){
+            return;
+        }
+        for(int i = j + 1; i < n; i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};

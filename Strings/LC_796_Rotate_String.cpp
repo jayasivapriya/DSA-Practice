36697988da9e9a717a01283419f9f6462613cd81
jayasivapriya <sivapriya2707@gmail.com>
2026-03-24 22:02:42 +0530

    Create LC_796_Rotate_String.cpp

---
+++ Strings/LC_796_Rotate_String.cpp
@@ -0,0 +1,31 @@
+/*
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+ 
+Constraints:
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        string doubledS = s + s;
+        return doubledS.find(goal) != string::npos;
+    }
+};

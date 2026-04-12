716c2d3d7513fa8a885a697b3bae726a57f37bcd
jayasivapriya <sivapriya2707@gmail.com>
2026-04-12 10:52:59 +0530

    Create LC_34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp

---
+++ Binary Search/LC_34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,68 @@
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+Example 3:
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+Constraints:
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+nums is a non-decreasing array.
+-10^9 <= target <= 10^9
+
+Time Complexity: O(2 * log n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int lowerBound(vector<int>& nums, int k){
+        int n = nums.size();
+        int low = 0, high = n - 1, ans = n;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] >= k){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid +1;
+            }
+        }
+        return ans;
+    }
+    int upperBound(vector<int>& nums, int k){
+        int n = nums.size();
+        int low = 0, high = n - 1, ans = n;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] > k){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid +1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int lb = lowerBound(nums, target);
+        if(lb == n || nums[lb] != target){
+            return {-1, -1};
+        }else{
+            return{lb, upperBound(nums, target) - 1};
+        }
+    }
+};
+
